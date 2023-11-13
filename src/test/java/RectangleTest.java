976f43b23a2fda7db42c0c4b3d9c35933185eb88
nedaei79 <nedaei79@gmail.com>
2023-11-13 19:38:09 +0330

    implement getter & setter tests

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -12,22 +12,28 @@ public class RectangleTest {
 
     @Test
     public void testGetHeight() {
-
+        Rectangle rectangle = new Rectangle(3, 4);
+        assertEquals("getHeight must give the actual height!", 3, rectangle.getHeight());
     }
 
     @Test
     public void testGetWidth() {
-
+        Rectangle rectangle = new Rectangle(3, 4);
+        assertEquals("getWidth must give the actual width!", 4, rectangle.getHeight());
     }
 
     @Test
     public void testSetHeight() {
-
+        Rectangle rectangle = new Rectangle(3, 4);
+        rectangle.setHeight(5);
+        assertEquals("setHeight must set the height as expected!", 5, rectangle.getHeight());
     }
 
     @Test
     public void testSetWidth() {
-
+        Rectangle rectangle = new Rectangle(3, 4);
+        rectangle.setWidth(6);
+        assertEquals("setWidth must set the width as expected!", 6, rectangle.getHeight());
     }
 
 }
