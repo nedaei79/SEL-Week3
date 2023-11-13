54961bd28ed0eb78891016c4c7d27714c5ad432b
nedaei79 <nedaei79@gmail.com>
2023-11-13 20:31:36 +0330

    implement Square methods so that the tests are passed

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -2,19 +2,19 @@ public class Square {
     private int length;
 
     public Square(int length) {
-
+        this.length = length;
     }
 
     public int computeArea() {
-        return 0;
+        return this.length * this.length;
     }
 
     public int getLength() {
-        return 0;
+        return this.length;
     }
 
     public void setLength(int length) {
-
+        this.length = length;
     }
 
 }
