0de519f67c844bc140eafbafa6c464377c3379cd
nedaei79 <nedaei79@gmail.com>
2023-11-13 19:25:41 +0330

    implement computeArea test

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -6,7 +6,8 @@ public class RectangleTest {
 
     @Test
     public void testComputeArea() {
-
+        Rectangle rectangle1 = new Rectangle(3, 4);
+        assertEquals("computeArea must compute area right!", 12, rectangle1.computeArea());
     }
 
 }
